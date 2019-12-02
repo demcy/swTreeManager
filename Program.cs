ec97957af71104125c791755f94813174cc75aad
demcy.meizu <demcy.meizu@gmail.com>
2019-12-02 15:51:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SW;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SwTools swTools = new SwTools();
+            Console.Write(swTools.swConnect());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SW/SwTools.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SW
+{
+    public class SwTools
+    {
+        public bool swConnect()
+        {
+            try
+            {
+                object swApp = Marshal.GetActiveObject("SldWorks.Application");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
